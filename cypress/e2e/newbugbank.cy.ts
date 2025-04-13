ebee3b0b5629d6fb262dcafa5a9711fc40d3e7e7
Pablo Bandeira <63755365+acerbityroque@users.noreply.github.com>
2025-04-13 20:28:14 -0300

    + transferência

--- cypress/e2e/newbugbank.cy.ts
+++ cypress/e2e/newbugbank.cy.ts
@@ -4,39 +4,34 @@ const credenciais = {
     senha: '123456'
 };
 
-beforeEach(() => {
-    // Visitar a página inicial
-    cy.visit('https://bugbank.netlify.app/');
-    // Clicar no botão "Registrar"
-    cy.get('button.style__ContainerButton-sc-1wsixal-0.ihdmxA').click({ force: true });
-});
+describe('Fluxo: Registro e Login', () => {
+    beforeEach(() => {
+        // Configurar registro e login antes de cada teste
+        cy.visit('https://bugbank.netlify.app/');
+        cy.get('button.style__ContainerButton-sc-1wsixal-0.ihdmxA').click({ force: true });
+        cy.get(':nth-child(2) > .input__default').type(credenciais.email, { force: true, delay: 100 });
+        cy.get('.input__default').eq(3).type(credenciais.nome, { force: true, delay: 100 });
+        cy.get('.input__default').eq(4).type(credenciais.senha, { force: true, delay: 100 });
+        cy.get('.input__default').eq(5).type(credenciais.senha, { force: true, delay: 100 });
+        cy.get('button.style__ContainerButton-sc-1wsixal-0.CMabB').click({ force: true });
+        cy.get('#toggleAddBalance').click({ force: true });
+        
+        cy.wait(3000);
+        cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > :nth-child(1) > .input__default').type(credenciais.email, { force: true, delay: 100 });
+        cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > .login__password > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').type(credenciais.senha, { force: true, delay: 100 });
+        cy.get('.otUnI').click({ force: true });
+    });
 
-it('Registro de novo usuário e login', () => {
-    // Registro de usuário
-    cy.get(':nth-child(2) > .input__default').type(credenciais.email, { force: true, delay: 100 });
-    cy.get('.input__default').eq(3).type(credenciais.nome, { force: true, delay: 100 });
-    cy.get('.input__default').eq(4).type(credenciais.senha, { force: true, delay: 100 });
-    cy.get('.input__default').eq(5).type(credenciais.senha, { force: true, delay: 100 });
-    cy.get('button.style__ContainerButton-sc-1wsixal-0.CMabB').click({ force: true });
-    cy.get('#toggleAddBalance').click({ force: true });
+    it('Realizar transferência', () => {
+        // Teste de transferência
+    });
 
-    // Verificar se o modal de sucesso é exibido e visível
-    cy.get('#btnCloseModal').should('be.visible');
+    it('Navegar para extrato', () => {
+        // Teste de extrato
+    });
 
-    // Pausa de 3 segundos antes de clicar no botão "Fechar Modal"
-    cy.wait(3000);
-
-    // Clicar no botão "Fechar Modal"
-    cy.get('#btnCloseModal').click({ force: true });
-
-    // Pausa de 3 segundos após fechar o modal
-    cy.wait(3000);
-
-    // Fazer login com as mesmas credenciais
-    cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > :nth-child(1) > .input__default').type(credenciais.email, { force: true, delay: 100 });
-    cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > .login__password > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').type(credenciais.senha, { force: true, delay: 100 });
-
-    // Clicar no botão "Acessar"
-    cy.get('.otUnI').click({ force: true });
-
+    it('Sair da aplicação', () => {
+        // Clicar no botão "Sair"
+        cy.get('#btnExit').click({ force: true });
+    });
 });
